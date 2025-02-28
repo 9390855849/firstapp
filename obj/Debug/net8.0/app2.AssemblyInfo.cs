0a1145953274a6675247d8485d1ba1aa45af87a9
priyanka <priyankapinky3536@gmail.com>
2025-02-28 12:26:46 +0530

    index folder updated

--- obj/Debug/net8.0/app2.AssemblyInfo.cs
+++ obj/Debug/net8.0/app2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9d96431f985c2d8f525b0cc0ad0b441d02e3bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("app2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
